f8ef4740929d75249f58fe207e3bfa223f3080bf
HeribertoCL11 <za240110714@zapopan.tecmm.edu.mx>
2025-09-25 00:48:57 -0600

    Create ingresa a,m,s.py

---
+++ PYTHON/ingresa a,m,s.py
@@ -0,0 +1,10 @@
+modo = input("Ingrese modo (A=Automático, M=Manual, S=Seguro): ")
+
+if modo == "A":
+    print("Modo AUTOMÁTICO")
+elif modo == "M":
+    print("Modo MANUAL")
+elif modo == "S":
+    print("Modo SEGURO")
+else:
+    print("Error: modo inválido")
