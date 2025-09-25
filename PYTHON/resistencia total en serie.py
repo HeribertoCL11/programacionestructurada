de9ec15e801f45893b0da2aacbdc296b10b9d71b
HeribertoCL11 <za240110714@zapopan.tecmm.edu.mx>
2025-09-24 23:22:50 -0600

    Create resistencia total en serie.py

---
+++ PYTHON/resistencia total en serie.py
@@ -0,0 +1,7 @@
+R1=int(input("valor de la primera resistencia"))
+R2=int(input("valor de la segunda resistencia"))
+R3=int(input("valor de la tercera resistencia"))
+
+RT=R1+R2+R3
+
+print('RESISTENCIA TOTAL =',RT)
