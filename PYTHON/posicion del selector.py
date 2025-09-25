c7372141e8b972fa80d8285a3aa7850efd2f5287
HeribertoCL11 <za240110714@zapopan.tecmm.edu.mx>
2025-09-25 00:49:04 -0600

    Create posicion del selector.py

---
+++ PYTHON/posicion del selector.py
@@ -0,0 +1,12 @@
+selector = int(input("Ingrese la posición del selector  "))
+
+if selector == 1:
+    print("Entrada 1 seleccionada")
+elif selector == 2:
+    print("Entrada 2 seleccionada")
+elif selector == 3:
+    print("Entrada 3 seleccionada")
+elif selector == 4:
+    print("Entrada 4 seleccionada")
+else:
+    print("Valor inválido")
