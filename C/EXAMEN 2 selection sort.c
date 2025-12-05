8e98219312c0daa8b1d76882cbcd1c6485adc1ee
HeribertoCL11 <za240110714@zapopan.tecmm.edu.mx>
2025-12-05 13:51:14 -0600

    Update EXAMEN 2 selection sort.c

--- C/EXAMEN 2 selection sort.c
+++ C/EXAMEN 2 selection sort.c
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include <stdio.h>
 
 #define NUM_PRODUCTOS 8
@@ -53,3 +54,60 @@ int main() {
 
     return 0;
 }
+=======
+#include <stdio.h>
+
+#define NUM_PRODUCTOS 8
+
+typedef struct {
+    char nombre[30];
+    float precio;
+    int vida_util;  // tiempo de vida
+} Producto;
+
+void selectionSort(Producto productos[], int n) {
+    int i, j, minIndex;
+    Producto temp;
+
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+        for (j = i + 1; j < n; j++) {
+            if (productos[j].precio < productos[minIndex].precio) {
+                minIndex = j;
+            }
+        }
+
+        temp = productos[i];
+        productos[i] = productos[minIndex];
+        productos[minIndex] = temp;
+    }
+}
+
+int main() {
+   
+    Producto productos[NUM_PRODUCTOS] = {
+        {"cable", 22.5, 12},
+        {"pinza", 15.0, 24},
+        {"desarmador", 15.5, 36},
+        {"gia", 100.0, 60},
+        {"multimetro", 25.5, 48},
+        {"cinta", 20.0, 6},
+        {"guantes", 50.70, 18},
+        {"casco de seguridad", 70.00, 30}
+    };
+
+    printf("Lista de productos:\n");
+    for (int i = 0; i < NUM_PRODUCTOS; i++) {
+        printf("%s - $%.2f - Vida útil: %d meses\n", productos[i].nombre, productos[i].precio, productos[i].vida_util);
+    }
+
+    selectionSort(productos, NUM_PRODUCTOS);
+
+    printf("\nLista de productos ordenados :\n");
+    for (int i = 0; i < NUM_PRODUCTOS; i++) {
+        printf("%s - $%.2f - Vida útil: %d meses\n", productos[i].nombre, productos[i].precio, productos[i].vida_util);
+    }
+
+    return 0;
+}
+>>>>>>> Stashed changes
