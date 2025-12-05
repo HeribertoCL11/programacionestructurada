37e0a3233bcc44e47372b5cfc9cd13fc22a22e34
HeribertoCL11 <za240110714@zapopan.tecmm.edu.mx>
2025-12-05 13:50:59 -0600

    Update cociente de dos numeros.c

---
+++ C/cociente de dos numeros.c
@@ -0,0 +1,17 @@
+// cociente de dos numeros
+#include <stdio.h>
+
+int main() {
+    float a;
+    float b;
+    float cociente;
+    printf("ingresa un numero");
+    scanf("%f",&a);
+    printf("ingresa otro numero");
+    scanf("%f",&b);
+    cociente=a/b;
+    printf("el cociente es =  %.2f\n",cociente);
+    
+
+    return 0;
+}
