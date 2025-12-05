c919d7cc3a6fd2f7f7dbea0eb341bf82a8aad20a
HeribertoCL11 <za240110714@zapopan.tecmm.edu.mx>
2025-12-05 13:51:10 -0600

    Update doble de un numero.c

---
+++ C/doble de un numero.c
@@ -0,0 +1,13 @@
+// doble de un numero
+#include <stdio.h>
+
+int main() {
+    float a;
+    float doble;
+    printf("ingresa un numero");
+    scanf("%f",&a);
+    doble=a*2;
+    printf("el doble de ese numero es  %.2f\n",doble);
+
+    return 0;
+}
