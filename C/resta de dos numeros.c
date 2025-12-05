798949f9dda054d0fc0fc4ebe7077c57d90b908d
HeribertoCL11 <za240110714@zapopan.tecmm.edu.mx>
2025-12-05 13:52:16 -0600

    Update resta de dos numeros.c

---
+++ C/resta de dos numeros.c
@@ -0,0 +1,16 @@
+// resta
+#include <stdio.h>
+
+int main() {
+    float a;
+    float b;
+    float resta;
+    printf("ingresa un numero");
+    scanf("%f",&a);
+    printf("ingresa otro numero");
+    scanf("%f",&b);
+    resta=a-b;
+    printf("la resta es =  %.2f\n",resta);
+
+    return 0;
+}
