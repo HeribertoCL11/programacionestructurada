983cf034b3801bcd9f9a89dbdf8988098c54caf2
HeribertoCL11 <za240110714@zapopan.tecmm.edu.mx>
2025-12-05 13:51:54 -0600

    Update perimetro de un cuadrado.c

---
+++ C/perimetro de un cuadrado.c
@@ -0,0 +1,13 @@
+// perimetro
+#include <stdio.h>
+
+int main() {
+    float a;
+    float total;
+     printf("lado del cuadrado:");
+    scanf("%f",&a);
+   total=4*a;
+    printf("el perimetro del cuadrado es =  %.2f\n",total);
+
+    return 0;
+}
