b2b8e1e3c0d174eb7e2c3f57979d4d91cf484e1b
HeribertoCL11 <za240110714@zapopan.tecmm.edu.mx>
2025-12-05 13:52:26 -0600

    Update suma de dos numeros.c

---
+++ C/suma de dos numeros.c
@@ -0,0 +1,16 @@
+// suma
+#include <stdio.h>
+
+int main() {
+    float a;
+    float b;
+    float suma;
+    printf("ingresa un numero");
+    scanf("%f",&a);
+    printf("ingresa otro numero");
+    scanf("%f",&b);
+    suma=a+b;
+    printf("la suma es =  %.2f\n",suma);
+
+    return 0;
+}
