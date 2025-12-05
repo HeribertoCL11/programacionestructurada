62601f0b68e87796dd6e00ef532a542ec28de2b1
HeribertoCL11 <za240110714@zapopan.tecmm.edu.mx>
2025-12-05 13:52:19 -0600

    Update salario mensual.c

---
+++ C/salario mensual.c
@@ -0,0 +1,13 @@
+// salario mensual
+#include <stdio.h>
+
+int main() {
+    float a;
+    float total;
+     printf("salario semanal:");
+    scanf("%f",&a);
+   total=4*a;
+    printf("salario total =  %.2f\n",total);
+
+    return 0;
+}
